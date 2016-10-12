c8717e5673880fbf0ad8430f5b72d2b2b75b254e
Ven Nguyen <vanven08ctt3@gmail.com>
2016-10-12 18:49:45 +0700

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'contact_us' => 'contact_us#index'
+
+  get 'menu' => 'menu#index'
+
+  resources :food_items
+  get 'home/contact_us'
+
+  root "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
